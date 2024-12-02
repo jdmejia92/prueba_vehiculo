1d82f59bd2077785ec55d350c8d02b81938781a7
Juan Mejia Hoyos <juanmejiahoyos@Juans-MacBook-Pro.local>
2024-12-01 23:30:30 -0500

    arreglo bluid 6

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python AutoGo_proyecto/manage.py collecstatic --noinput
+python AutoGo_proyecto/manage.py collectstatic --noinput
 python AutoGo_proyecto/manage.py migrate
